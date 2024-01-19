927b491504ed33eb2df6ebfb5208fdb75039c60b
saba <89579913+sabaFitwi@users.noreply.github.com>
2024-01-19 14:56:39 +0100

    Add Api links

---
+++ src/services/api/index.jsx
@@ -0,0 +1,18 @@
+const baseUrl = "https://api.noroff.dev/api/v1/holidaze";
+const loginUrl = "https://api.noroff.dev/api/v1/holidaze/auth/login";
+const registerUrl = "https://api.noroff.dev/api/v1/holidaze/auth/register";
+const getAllVenues =
+  "https://api.noroff.dev/api/v1/holidaze/venues?_bookings=true&_owner=true&sort=created&sortOrder=desc";
+const bookingsUrl = "https://api.noroff.dev/api/v1/holidaze/bookings";
+const profileUrl = "https://api.noroff.dev/api/v1/holidaze/profiles";
+const createVenueUrl = "https://api.noroff.dev/api/v1/holidaze/venues";
+
+export {
+  loginUrl,
+  registerUrl,
+  getAllVenues,
+  bookingsUrl,
+  profileUrl,
+  createVenueUrl,
+  baseUrl,
+};
