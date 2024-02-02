8d29bdac8768a1114215dce930830c5770a47ef3
saba <89579913+sabaFitwi@users.noreply.github.com>
2024-02-02 15:56:45 +0100

    Add Search functionality

---
+++ src/Data/ContinentsData.jsx
@@ -0,0 +1,39 @@
+import australiaImg from "../assets/cont/12.png";
+import africaImg from "../assets/cont/7.png";
+import asiaImg from "../assets/cont/9.png";
+import europaImg from "../assets/cont/1.png";
+import northAmericaImg from "../assets/cont/4.png";
+import southAmericaImg from "../assets/cont/14.png";
+
+const sorting = [
+  {
+    name: "All",
+    image: null,
+  },
+  {
+    name: "Africa",
+    image: africaImg,
+  },
+  {
+    name: "Asia",
+    image: asiaImg,
+  },
+  {
+    name: "Australia",
+    image: australiaImg,
+  },
+  {
+    name: "Europe",
+    image: europaImg,
+  },
+  {
+    name: "North America",
+    image: northAmericaImg,
+  },
+  {
+    name: "South America",
+    image: southAmericaImg,
+  },
+];
+
+export default sorting;
