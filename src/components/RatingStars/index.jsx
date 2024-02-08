61744799bb0825641678753bdb402a6754f52adc
saba <89579913+sabaFitwi@users.noreply.github.com>
2024-02-08 14:25:52 +0100

    Add rating

---
+++ src/components/RatingStars/index.jsx
@@ -0,0 +1,36 @@
+// StarRating.js
+import React from "react";
+import {
+  TiStarFullOutline,
+  TiStarHalfOutline,
+  TiStarOutline,
+} from "react-icons/ti";
+
+function StarRating({ rating }) {
+  const stars = [];
+  const fullStars = Math.floor(rating);
+  const hasHalfStar = rating % 1 !== 0;
+
+  for (let i = 0; i < fullStars; i++) {
+    stars.push(
+      <TiStarFullOutline key={i} className="text-yellow-500 text-sm" />,
+    );
+  }
+
+  if (hasHalfStar) {
+    stars.push(
+      <TiStarHalfOutline key={fullStars} className="text-yellow-500 text-sm" />,
+    );
+  }
+
+  const remainingStars = 5 - stars.length;
+  for (let i = 0; i < remainingStars; i++) {
+    stars.push(
+      <TiStarOutline key={fullStars + i} className="text-yellow-500 text-sm" />,
+    );
+  }
+
+  return <div className="flex">{stars}</div>;
+}
+
+export default StarRating;
