a1a50ef8975fa116e2a1b0877bb8ec327a9b1f05
saba <89579913+sabaFitwi@users.noreply.github.com>
2024-02-25 02:26:09 +0100

    update

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -13,21 +13,21 @@ function PageNotFound() {
         description="Oops! It looks like you've wandered off the beaten path. Return to Holidaze's homepage and continue your journey to find the perfect accommodation for your next adventure."
       />
       <main>
-        <div class="h-screen w-screen bg-gray-100 flex items-center">
-          <div class="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
-            <div class="max-w-md">
-              <div class="text-5xl font-dark font-bold">404</div>
-              <p class="text-2xl md:text-3xl font-light leading-normal">
+        <div className="h-screen w-screen  flex items-center">
+          <div className="container flex flex-col md:flex-row items-center justify-center px-5 dark:text-white text-gray-700">
+            <div className="max-w-md">
+              <div className="text-5xl font-dark font-bold">404</div>
+              <p className="text-2xl md:text-3xl font-light leading-normal">
                 Sorry we couldn't find this page.{" "}
               </p>
-              <p class="mb-8">
+              <p className="mb-8">
                 But dont worry, you can find plenty of other things on our
                 homepage.
               </p>
 
               <Link
                 to={"./"}
-                class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
+                className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-primary  hover:bg-secondary"
               >
                 back to homepage
               </Link>
